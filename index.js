3e32cb0511ebb7ca35f3c063b645c25c485c24ab
AndriiShnurkov <Shans12.84@ukr.net>
2020-04-02 10:56:00 +0300

    cookies

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict'
+
+
+// const json = `{
+// "name": "Ivan",
+//     "age": 25,
+//         "status": "student",
+//             "online": true,
+//                 "header": ["Home", "About", "Contacts"]
+// }`;
+
+
+// Method JSON.parse()
+// const result = JSON.parse(json);
+// console.log(result);
+
+// =========== JSON.parse(*****) ==============
+
+// function markupFromString(data) {
+
+// let string = '';
+// array.forEach(element => {
+//     string += `
+//         <li>${element}</li>
+//         `
+// });
+// return string;
+
+// ========== 2 ==========
+
+// return JSON.parse(data).reduce((accum, item) => {
+//     accum += `<li>${item}</li>`
+//     return accum;
+// }, '')
+// }
+
+// const data = '["Home", "About", "Contacts"]'; //string
+
+// const list = document.querySelector(".list");
+// list.innerHTML = markupFromString(data);
+
+
+// ============= JSON.stringify( ****** ) ==================
+
+// const user = {
+//     name: "Ivan",
+//     age: 25,
+//     status: "student",
+//     online: true,
+// };
+
+// const json = JSON.stringify(user);
+// console.log(json);
+
+// ============= cookies ====  4096 символов ==========
+
+// const age = document.cookie.match(/age=(.+?);/);
+
+// document.cookie = "age=25";
+// document.cookie = `user=${json}`
+// console.log(document.cookie);
+
+// document.cookie = "name=Andrii";
+// console.log(document.cookie);
+
+// ============== 
+
+
+
+
+
+
+
