115d727c503d0a3fbd6dcd3cd934acc768ef3cb9
AndriiShnurkov <Shans12.84@ukr.net>
2020-04-02 13:18:53 +0300

    ex

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 // const json = `{
 // "name": "Ivan",
 //     "age": 25,
@@ -52,7 +51,7 @@
 // const json = JSON.stringify(user);
 // console.log(json);
 
-// ============= cookies ====  4096 символов ==========
+// ============= cookies ====  4096 символов ===== 1 кук = 30 - 70 символов =====
 
 // const age = document.cookie.match(/age=(.+?);/);
 
@@ -63,7 +62,117 @@
 // document.cookie = "name=Andrii";
 // console.log(document.cookie);
 
-// ============== 
+// ============== localStorage ========= 50 - 12 мегабайт =====
+
+// localStorage.setItem("key", "Value");
+
+// localStorage.getItem("key");
+
+// localStorage.removeItem("key");
+
+// localStorage.clear();
+
+
+// ============== ex1 ================
+// localStorage.setItem("name", "Andrii");
+// console.log(localStorage.getItem("name"));
+// localStorage.clear();
+
+// ============== ex2 ================
+// const myObject = {
+//     name: "Andrii",
+//     parametrs: [10, 20, 30]
+// }
+
+// const json = JSON.stringify(myObject);
+// localStorage.setItem("user", json);
+
+// const getJson = localStorage.getItem('user'); //string
+// const convertJSon = JSON.parse(getJson); // object
+// console.log(getJson);
+// console.log(convertJSon);
+
+// exp1
+
+const object = `{
+    "menu": ["Home", "About", "Categories"],
+    "categories": ["Products", "Tools", "Cars", "Homes"]
+}`
+
+// const constructor = {
+//     method(data) {
+//         const parseData = JSON.parse(data); //'' => {}
+//         const keys = Object.keys(parseData); // ["menu", "categories"]
+//         keys.forEach(key => {
+//             this.templateItems(parseData[key], key)// parseData.key
+//         })
+//     },
+//     templateItems(array, str) { //["Home", "About", "Categories"], menu
+//         const buildStr = array.reduce((acc, item) => {
+//             return acc += `<li data-target="li">${item}</li>`
+//         }, '') //<li data-target="li">Home</li><li data-target="li">About</li><li data-target="li">Categories</li>
+//         const element = document.querySelector(`.${str}`); //ul
+//         element.addEventListener('click', this.handleClick);
+//         element.innerHTML = buildStr;
+//     },
+//     handleClick(e) {
+//         const target = e.target;
+//         // if (e.target === e.currentTarget) {
+//         //     return
+//         // }
+//         // if (e.target.nodeName !== "LI") {
+//         //     return
+//         // }
+//         if (e.target.dataset.target === 'li') {
+//             console.log(target.textContent);
+//         }
+//     },
+// }
+
+// constructor.method(object)
+
+
+function constructor(data) {
+    (function method(data) {
+        const parseData = JSON.parse(data); //'' => {}
+        const keys = Object.keys(parseData); // ["menu", "categories"]
+        keys.forEach(key => {
+            templateItems(parseData[key], key)// parseData.key
+        })
+    })(data)
+    function templateItems(array, str) { //["Home", "About", "Categories"], menu
+        const buildStr = array.reduce((acc, item) => {
+            return acc += `<li data-target="li">${item}</li>`
+        }, '') //<li data-target="li">Home</li><li data-target="li">About</li><li data-target="li">Categories</li>
+        const element = document.querySelector(`.${str}`); //ul
+        element.addEventListener('click', handleClick);
+        element.innerHTML = buildStr;
+    }
+    function handleClick(e) {
+        const target = e.target;
+        // if (e.target === e.currentTarget) {
+        //     return
+        // }
+        // if (e.target.nodeName !== "LI") {
+        //     return
+        // }
+        if (e.target.dataset.target === 'li') {
+            console.log(target.textContent);
+        }
+    }
+}
+
+constructor(object)
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ regExp.js
@@ -0,0 +1,35 @@
+
+
+// const string = 'Lolofsdorem ipsum, dolor sit amet consecotetur adipisicing eloloit. ';
+
+// const str = string.replace(/o/g, "*");
+// const str = string.replace(/o/, "*")
+// console.log(str);
+
+// const str = string.replace(/o.o/g, "*") //. - символ между о и о
+// console.log(str);
+
+
+// const string = "xczz zxcxzc zxcxc zxcxcz xzccxz xzc"
+
+// const res = string.replace(/xc+x/g, "*")
+// console.log(res);
+
+// const regObject = `
+// {
+//     "path": "https://regexp.com.ua",
+//     "number": {
+//         "path": "https://regexp?adventure="fggjvj".com.ua"
+//     } 
+// }`
+
+// const result = regObject.replace(/path/g, 'oldPath')
+// console.log('result :', result);
+
+// const str = "xx xax xxaax xaaax xx xax xaax xxxxxxxxxxxxxxxxXXAAaaaaaaaxxx xax xxaax xaaax"
+// let result = str.replace(/X{1,2}A{1,2}/gi, '*');
+// console.log(result);
+
+
+
+
